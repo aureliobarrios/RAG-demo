2e40a08d7877842341112fc1c4755123d4dd0ec2
aubarrio <aubarrios142@gmail.com>
2025-03-26 20:03:59 -0700

    additional imports

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import os
 from groq import Groq
 from langchain.schema.document import Document
 from langchain.prompts import ChatPromptTemplate
@@ -5,3 +6,4 @@ from langchain.vectorstores.chroma import Chroma
 from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
