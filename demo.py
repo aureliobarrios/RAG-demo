ba88d14dfb8a78e1f8a79ac848ec8cac8b48d609
aubarrio <aubarrios142@gmail.com>
2025-03-26 20:07:09 -0700

    groq invoke

--- demo.py
+++ demo.py
@@ -11,3 +11,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+#function to invoke groq response
+def groq_invoke(prompt):
+    
+    client = Groq(
+        api_key=GROQ_API_KEY,
+    )
+    
+    chat_completion = client.chat.completions.create(
+        messages = [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        model = "llama3-8b-8192"
+    )
+    
+    return chat_completion.choices[0].message.content
